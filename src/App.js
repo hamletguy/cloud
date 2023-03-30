63071b0b67b1a47744b2cfeaeaa06fb2307c5527
ahmed ayachi <ahmedayachi1607@gmail.com>
2023-03-30 16:47:38 +0100

    google maps isnt available

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Maps from './components/maps';
+
+function App() {
+  return (
+    <div className="App">
+      <Maps></Maps>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Maps.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Maps() {
+  return (
+    <div>maps unavailable need $$</div>
+  )
+}
+
+export default Maps
